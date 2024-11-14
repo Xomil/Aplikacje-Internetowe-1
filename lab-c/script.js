cb316a155eb40fa28f66936d9c4c21596cdb10ef
Damian <xomilol@gmail.com>
2024-11-14 18:47:38 +0100

    cos

--- lab-c/script.js
+++ lab-c/script.js
@@ -1,6 +1,13 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+function Locate(){
+    if(!navigator.geolocation){
+        alert("Geolocation is not supported by this browser.");
+        
+    }
+    navigator.geolocation.getCurrentPosition(showPosition);
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+    var latlon = lat + "," + lon;
+    
+    
+    
+}
