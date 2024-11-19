f121ad27a2163ede255f053a6653a661cc4f8295
Xomil <xomilol@gmail.com>
2024-11-19 18:00:53 +0100

    elo

--- lab-c/script.js
+++ lab-c/script.js
@@ -3,11 +3,26 @@ function Locate(){
         alert("Geolocation is not supported by this browser.");
         
     }
-    navigator.geolocation.getCurrentPosition(showPosition);
-    var lat = position.coords.latitude;
-    var lon = position.coords.longitude;
-    var latlon = lat + "," + lon;
+    navigator.geolocation.getCurrentPosition((position) => {
+        var lat = position.coords.latitude;
+        var lon = position.coords.longitude
+        document.getElementById("lat").innerText = lat;
+        document.getElementById("lon").innerText = lon;
+        map.setView([lat, lon], 25);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    }, (positionError) => {
+        console.error(positionError);
+    });
     
     
     
 }
+
+var map = L.map("map").setView([53.44717632281161, 14.4919321], 25);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
